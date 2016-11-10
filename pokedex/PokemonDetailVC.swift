3ed1306d308cc080ff968cc2d3c7028e9b96a0f5
Martyn Cheatle <Martyn@Martyns-MacBook-Pro.local>
2016-11-10 19:02:45 +0000

    Should have added before damn

--- pokedex/PokemonDetailVC.swift
+++ pokedex/PokemonDetailVC.swift
@@ -2,77 +2,34 @@
 //  PokemonDetailVC.swift
 //  pokedex
 //
-//  Created by Martyn Cheatle on 07/11/2016.
+//  Created by Martyn Cheatle on 10/11/2016.
 //  Copyright © 2016 Martyn Cheatle. All rights reserved.
 //
 
 import UIKit
-import Foundation
 
 class PokemonDetailVC: UIViewController {
-    
-    var pokemon: Pokemon!
-    
-    @IBOutlet weak var nameLbl: UILabel!
-    @IBOutlet weak var mainImg: UIImageView!
-    @IBOutlet weak var descriptionLbl: UILabel!
-    @IBOutlet weak var typeLbl: UILabel!
-    @IBOutlet weak var defenseLbl: UILabel!
-    @IBOutlet weak var heightLbl: UILabel!
-    @IBOutlet weak var pokedexLbl: UILabel!
-    @IBOutlet weak var weightLbl: UILabel!
-    @IBOutlet weak var attackLbl: UILabel!
-    @IBOutlet weak var currentEvoImg: UIImageView!
-    @IBOutlet weak var nextEvoImg: UIImageView!
-    @IBOutlet weak var evoLbl: UILabel!
-    
-    
-    
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        nameLbl.text = pokemon.name.capitalized
-        
-        let img = UIImage(named: "\(pokemon.pokedexId)")
-        
-        mainImg.image = img
-        currentEvoImg.image = img
-        pokedexLbl.text = "\(pokemon.pokedexId)"
-        pokemon.downloadPokemonDetail {
-            
-            // Whatever we write will only be called after the network call is compelte!
-            self.updateUI()
-        }
-        
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
     }
     
-    func updateUI() {
-        
-        attackLbl.text = pokemon.attack
-        defenseLbl.text = pokemon.defence
-        heightLbl.text = pokemon.height
-        weightLbl.text = pokemon.weight
-        typeLbl.text = pokemon.type
-        descriptionLbl.text = pokemon.description
-        
-        if pokemon.nextEvolutionId == "" {
-            
-            evoLbl.text = "No Evolutions"
-            nextEvoImg.isHidden = true
-            
-        } else {
-            
-            nextEvoImg.isHidden = false
-            nextEvoImg.image = UIImage(named: pokemon.nextEvolutionId)
-            let str = "Next Evolution: \(pokemon.nextEvolutionName) - LVL \(pokemon.nextEvolutionLevel)"
-            evoLbl.text = str
-        }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
     }
-    
-    @IBAction func backBtnPressed(_ sender: UIButton) {
-        
-        dismiss(animated: true, completion: nil)
-    }
-    
+    */
+
 }
